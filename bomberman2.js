c6b194ebdec4fe0c5af56d024a1b3c27f06bd308
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-05-31 03:25:42 +0530

    add bomberman2.js

--- bomberman2.js
+++ bomberman2.js
@@ -16,7 +16,7 @@ function startGame(){
     document.querySelector('.score').innerHTML = `score : ${score}`
     bombArray = []
     createArray()
-    console.log(bombArray)
+    //console.log(bombArray)
     cellElements.forEach(cell =>{
         cell.classList.remove(redselect,greenselect,"disabled");
         cell.innerHTML = "";
@@ -28,11 +28,12 @@ function startGame(){
 }
 
 function toClickClass(e){
+    //console.log(e.target.id)
     let cell = e.target
-    console.log(cell)
+    //console.log(cell)
     let flag = false
     let bomb_val = parseInt(cell.id)
-    console.log(typeof bomb_val)
+    //console.log(typeof bomb_val)
     
     if (bombArray.includes(bomb_val)){ flag = true;}
     // bombArray.forEach(curr =>{
@@ -47,13 +48,33 @@ function toClickClass(e){
 }
 function warningBombClass(e){
     //console.log('clicked')
+    //console.log(e.target);
     e.target.innerHTML = "!"
+    let node = document.createElement("span")
+    let textnode = document.createTextNode("bomb might be there")
+    node.appendChild(textnode)
+    node.className="child"
+    document.getElementById(e.target.id).appendChild(node);
+
+
+    e.target.addEventListener("mouseover",(event)=>{
+        //console.log(e.target)
+        node.classList.add("show")
+    })
+    e.target.addEventListener("mouseout",(event)=>{
+        node.classList.remove("show")
+    })
+    console.log(e.target)
+
+    //document.querySelector('.warningdisplayhover').classList.add('show')
+    //console.log(e.target)
+    
 }
 function checkClass(cell,flag,bomb_val){
     if (flag){
         
         makeDisable()
-        console.log(cell)
+        //console.log(cell)
         //cell.classList.add(redselect)
         //console.log(cell)
         document.getElementById('bomb').classList.add('show')
@@ -77,11 +98,21 @@ function checkClass(cell,flag,bomb_val){
         // setTimeout(function(){
         //     document.getElementById('emoji'+r).classList.remove('show')
         // },500)
-        console.log('first time clicked')
+        //console.log('first time clicked')
         cell.classList.add(greenselect)
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("one")
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            
+            event.target.classList.remove("one")
+            //console.log(cell)
+
+        })
         score++
         document.querySelector('.score').innerHTML = `score : ${score}`
-        let noB = noOfBombs(bomb_val)
+        let noB = noOfBombs(bomb_val,cell)
         cell.innerHTML = `${noB}`
         
         if (score === 72){
@@ -109,7 +140,7 @@ function winningText(score){
 
         
     }
-    console.log(winningMessageDisplay)
+    //console.log(winningMessageDisplay)
     setTimeout(function(){
         winningMessage.classList.add('show')
     },3000)
@@ -122,7 +153,7 @@ function makeDisable(){
 };
 
 function bombDisplay(){
-    console.log(document.getElementById('bomb'))
+    //console.log(document.getElementById('bomb'))
     document.getElementById('bomb').classList.add('show')
 }
 function displayBombs(){
@@ -161,41 +192,104 @@ function createArray(){
 function hideBombs(){
     bombArray.forEach(cell=>{
         document.getElementById(cell).classList.remove(redselect);
-        console.log(document.getElementById(cell))
+        //console.log(document.getElementById(cell))
     })
 }
 
-function noOfBombs(bomb_val)
+function noOfBombs(bomb_val,cell)
 {
+    //console.log(cell)
     let temp = bomb_val;
     let val = 0;
-    console.log(typeof temp,temp)
+    //console.log(typeof temp,temp)
 
     //CORNER CASES
     if (temp === 1){
-        console.log("1hi")
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("firstedge")
+            document.getElementById(temp+1).classList.add("firstedgeright")
+            document.getElementById(temp+9).classList.add("firstedgedown")
+            document.getElementById(temp+10).classList.add("firstedgedownright")
+            console.log(document.getElementById(temp+1))
+            console.log(document.getElementById(temp+9))
+            console.log(document.getElementById(temp+10))
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            event.target.classList.remove("firstedge")
+            document.getElementById(temp+1).classList.remove("firstedgeright")
+            document.getElementById(temp+9).classList.remove("firstedgedown")
+            document.getElementById(temp+10).classList.remove("firstedgedownright")
+            
+        })
+        
         if (bombArray.includes(temp+1)){val++;}
         if (bombArray.includes(temp+9)){val++;}
         if (bombArray.includes(temp+10)){val++;}
+        //console.log(cell)
         return val;
     }
     //console.log(val)
     if (temp === 9){
-        console.log("9")
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("secondedge")
+            document.getElementById(temp-1).classList.add("secondedgeleft")
+            document.getElementById(temp+9).classList.add("secondedgedown")
+            document.getElementById(temp+8).classList.add("secondedgedownleft")
+            
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            event.target.classList.remove("secondedge")
+            document.getElementById(temp-1).classList.remove("secondedgeleft")
+            document.getElementById(temp+9).classList.remove("secondedgedown")
+            document.getElementById(temp+8).classList.remove("secondedgedownleft")
+            
+        })
         if (bombArray.includes(temp-1)){val++;}
         if (bombArray.includes(temp+8)){val++;}
         if (bombArray.includes(temp+9)){val++;}
         return val;
     }
     if (temp === 73){
-        console.log("73")
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("thirdedge")
+            document.getElementById(temp+1).classList.add("thirdedgeright")
+            document.getElementById(temp-9).classList.add("thirdedgeup")
+            document.getElementById(temp-8).classList.add("thirdedgeupright")
+            
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            //console.log(cell)
+            event.target.classList.remove("thirdedge")
+            document.getElementById(temp+1).classList.remove("thirdedgeright")
+            document.getElementById(temp-9).classList.remove("thirdedgeup")
+            document.getElementById(temp-8).classList.remove("thirdedgeupright")
+            
+        })
         if (bombArray.includes(temp-9)){val++;}
         if (bombArray.includes(temp-8)){val++;}
         if (bombArray.includes(temp+1)){val++;}
         return val;
     }
     if (temp === 81){
-        console.log("81")
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("fourthedge")
+            document.getElementById(temp-1).classList.add("fourthedgeleft")
+            document.getElementById(temp-9).classList.add("fourthedgeup")
+            document.getElementById(temp-10).classList.add("fourthedgeupleft")
+            
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            //console.log(cell)
+            event.target.classList.remove("fourthedge")
+            document.getElementById(temp-1).classList.remove("fourthedgeleft")
+            document.getElementById(temp-9).classList.remove("fourthedgeup")
+            document.getElementById(temp-10).classList.remove("fourthedgeupleft")
+            
+        })
         if (bombArray.includes(temp-10)){val++;}
         if (bombArray.includes(temp-9)){val++;}
         if (bombArray.includes(temp-1)){val++;}
@@ -204,6 +298,27 @@ function noOfBombs(bomb_val)
     //Edge CASES
     //Left
     if (temp % 9 == 1){
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("leftedge")
+            document.getElementById(temp+1).classList.add("leftedgeright")
+            document.getElementById(temp+9).classList.add("leftedgedown")
+            document.getElementById(temp+10).classList.add("leftedgedownright")
+            document.getElementById(temp-8).classList.add("leftedgeupright")
+            document.getElementById(temp-9).classList.add("leftedgeup")
+            
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            //console.log(cell)
+            event.target.classList.remove("leftedge")
+            document.getElementById(temp+1).classList.remove("leftedgeright")
+            document.getElementById(temp+9).classList.remove("leftedgedown")
+            document.getElementById(temp+10).classList.remove("leftedgedownright")
+            document.getElementById(temp-8).classList.remove("leftedgeupright")
+            document.getElementById(temp-9).classList.remove("leftedgeup")
+            
+        })
+
         if (bombArray.includes(temp-9)){val++;}
         if (bombArray.includes(temp-8)){val++;}
         if (bombArray.includes(temp+1)){val++;}
@@ -213,6 +328,26 @@ function noOfBombs(bomb_val)
     }
     //Top
     if ((Math.floor(temp/9)) == 0){
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("topedge")
+            document.getElementById(temp+1).classList.add("topedgeright")
+            document.getElementById(temp+9).classList.add("topedgedown")
+            document.getElementById(temp+10).classList.add("topedgedownright")
+            document.getElementById(temp+8).classList.add("topedgedownleft")
+            document.getElementById(temp-1).classList.add("topedgeleft")
+            
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            //console.log(cell)
+            event.target.classList.remove("topedge")
+            document.getElementById(temp+1).classList.remove("topedgeright")
+            document.getElementById(temp+9).classList.remove("topedgedown")
+            document.getElementById(temp+10).classList.remove("topedgedownright")
+            document.getElementById(temp+8).classList.remove("topedgedownleft")
+            document.getElementById(temp-1).classList.remove("topedgeleft")
+            
+        })
         if (bombArray.includes(temp-1)){val++;}
         if (bombArray.includes(temp+1)){val++;}
         if (bombArray.includes(temp+8)){val++;}
@@ -222,6 +357,26 @@ function noOfBombs(bomb_val)
     }
     //Right
     if (temp % 9 == 0){
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("rightedge")
+            document.getElementById(temp+9).classList.add("rightedgedown")
+            document.getElementById(temp-9).classList.add("rightedgeup")
+            document.getElementById(temp-1).classList.add("rightedgeleft")
+            document.getElementById(temp-10).classList.add("rightedgeleftup")
+            document.getElementById(temp+8).classList.add("rightedgeleftdown")
+            
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            //console.log(cell)
+            event.target.classList.remove("rightedge")
+            document.getElementById(temp+9).classList.remove("rightedgedown")
+            document.getElementById(temp-9).classList.remove("rightedgeup")
+            document.getElementById(temp-1).classList.remove("rightedgeleft")
+            document.getElementById(temp-10).classList.remove("rightedgeleftup")
+            document.getElementById(temp+8).classList.remove("rightedgeleftdown")
+            
+        })
         if (bombArray.includes(temp-10)){val++;}
         if (bombArray.includes(temp-9)){val++;}
         if (bombArray.includes(temp-1)){val++;}
@@ -229,7 +384,28 @@ function noOfBombs(bomb_val)
         if (bombArray.includes(temp+9)){val++;}
         return val;
     }
+    //Bottom
     if ((Math.ceil(temp/9)) == 9){
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("bottomedge")
+            document.getElementById(temp-1).classList.add("bottomleft")
+            document.getElementById(temp+1).classList.add("bottomright")
+            document.getElementById(temp-9).classList.add("bottomup")
+            document.getElementById(temp-10).classList.add("bottomupleft")
+            document.getElementById(temp-8).classList.add("bottomupright")
+            
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            //console.log(cell)
+            event.target.classList.remove("bottomedge")
+            document.getElementById(temp-1).classList.remove("bottomleft")
+            document.getElementById(temp+1).classList.remove("bottomright")
+            document.getElementById(temp-9).classList.remove("bottomup")
+            document.getElementById(temp-10).classList.remove("bottomupleft")
+            document.getElementById(temp-8).classList.remove("bottomupright")
+            
+        })
         if (bombArray.includes(temp-10)){val++;}
         if (bombArray.includes(temp-9)){val++;}
         if (bombArray.includes(temp-8)){val++;}
@@ -238,6 +414,32 @@ function noOfBombs(bomb_val)
         return val;
     }
     else{
+        cell.addEventListener('mouseover',(event)=>{
+            //console.log(cell)
+            event.target.classList.add("middle")
+            document.getElementById(temp-1).classList.add("middleleft")
+            document.getElementById(temp+1).classList.add("middleright")
+            document.getElementById(temp-9).classList.add("middleup")
+            document.getElementById(temp-8).classList.add("middleupright")
+            document.getElementById(temp-10).classList.add("middleupleft")
+            document.getElementById(temp+8).classList.add("middledownleft")
+            document.getElementById(temp+9).classList.add("middledown")
+            document.getElementById(temp+10).classList.add("middledownright")
+            
+        })
+        cell.addEventListener('mouseout',(event)=>{
+            //console.log(cell)
+            event.target.classList.remove("middle")
+            document.getElementById(temp-1).classList.remove("middleleft")
+            document.getElementById(temp+1).classList.remove("middleright")
+            document.getElementById(temp-9).classList.remove("middleup")
+            document.getElementById(temp-8).classList.remove("middleupright")
+            document.getElementById(temp-10).classList.remove("middleupleft")
+            document.getElementById(temp+8).classList.remove("middledownleft")
+            document.getElementById(temp+9).classList.remove("middledown")
+            document.getElementById(temp+10).classList.remove("middledownright")
+            
+        })
         if (bombArray.includes(temp-10)){val++;}
         if (bombArray.includes(temp-9)){val++;}
         if (bombArray.includes(temp-8)){val++;}
@@ -252,4 +454,15 @@ function noOfBombs(bomb_val)
     //console.log(val)
 }
 
+
+document.querySelectorAll(".greenselect").forEach(cell=>{
+    console.log(cell)
+    cell.addEventListener('mouseover',(event)=>{
+        console.log(event.target)
+        event.target.getElementById('id').classList.add('borderhover')
+    })
+    cell.addEventListener('mouseout',(event)=>{
+        event.target.classList.remove('borderhover')
+    })
+})
 
