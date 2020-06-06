6d2566f9810c9f8a7c8713d11e626e5320db0f0f
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-06-07 02:27:06 +0530

    updated code

--- bomberman2.js
+++ bomberman2.js
@@ -1,4 +1,5 @@
 let bombArray;
+let newBombArray;
 let score  = 0
 const redselect = 'redselect'
 const greenselect = 'greenselect'
@@ -8,75 +9,53 @@ const winningMessage = document.getElementById('winningMessage')
 const restartButtton = document.getElementById("restartButton")
 const scoreElement = document.querySelector(".scoreElement")
 const resetButtton = document.querySelector(".restartButton")
-//console.log(document.querySelector('body'))
+
+// Start Game
 startGame()
+// Restart Game
 restartButton.addEventListener('click',startGame);
+// Reset Game
 resetButton.addEventListener('click',startGame);
+
+// Game Start Function
 function startGame(){
-    console.log(document.querySelector('body'))
     score = 0
     let h = ""
     document.querySelector('.score').innerHTML = `score : ${score}`
     bombArray = []
     createArray()
-    //console.log(bombArray)
+    newBombArray = []
+    changeBombArray()
     cellElements.forEach(cell =>{
         cell.classList.remove(redselect,greenselect,"disabled","warning");
-        //cell.classList.remove("firstedge","firstedgeright","firstedgedown","firstedgedownright")
         cell.innerHTML = "";
         cell.removeEventListener('click',toClickClass)
         cell.removeEventListener('mouseover',highlightClass)
         cell.removeEventListener('mouseout',highlightClass1)
-
-
-
         cell.addEventListener('click',toClickClass,{once:true})
         cell.addEventListener('contextmenu',warningBombClass,{once:true})
-        // cell.addEventListener('mouseover',edge)
-        // cell.removeEventListener('mouseout',edge1)
     })
     winningMessage.classList.remove("show")
 }
 
+// On Clicking event
 function toClickClass(e){
-    //console.log(e.target.id)
+    e.preventDefault();
     let cell = e.target
-    //console.log(cell)
     let flag = false
     let bomb_val = parseInt(cell.id)
-    //console.log(typeof bomb_val)
-    
     if (bombArray.includes(bomb_val)){ flag = true;}
-
     checkClass(cell,flag,bomb_val);
-    //console.log(fail)
 }
-function warningBombClass(e){
-    //console.log('clicked')
-    //console.log(e.target);
-    e.target.classList.add("warning")
-    let node = document.createElement("span")
-    let textnode = document.createTextNode("bomb might be there")
-    node.appendChild(textnode)
-    node.className="child"
-    document.getElementById(e.target.id).appendChild(node);
 
-}
+// Check whether bomb was there or not
 function checkClass(cell,flag,bomb_val){
     if (flag){
-        
         makeDisable()
-        
-        document.getElementById('bomb').classList.add('show')
-        setTimeout(function(){
-            document.getElementById('bomb').classList.remove('show')
-        },500)
+       
         cell.classList.add(redselect)
         displayBombs()
-    
         winningText(score)
-        //displayBombs()
-        
     }
     else{
         cell.classList.remove("warning")
@@ -96,54 +75,64 @@ function checkClass(cell,flag,bomb_val){
     }
 }
 
+// winning text displays
 function winningText(score){
-    //console.log(score)
     if (score === 72){
         document.querySelector('.score').innerHTML = `score : ${score}`
         scoreElement.innerHTML = `score : ${score}`
         winningMessageDisplay.innerHTML = 'congratulations you won'
-        //console.log("1")
         
     }
     else{
         document.querySelector('.score').innerHTML = `score : ${score}`
         scoreElement.innerHTML = `score : ${score}`
         winningMessageDisplay.innerHTML = 'Best of luck'
-        //console.log("2")        
+          
     }
-    //console.log(winningMessageDisplay)
     setTimeout(function(){
         winningMessage.classList.add('show')
     },3000)
     
 }
+//disable clicking functionality
 function makeDisable(){
     cellElements.forEach(cell => {
         cell.classList.add("disabled");
     });
 };
 
-function bombDisplay(){
-    //console.log(document.getElementById('bomb'))
-    document.getElementById('bomb').classList.add('show')
+//Flag display
+function warningBombClass(e){
+    e.preventDefault();
+    if(e.target.classList.contains("greenselect")){
+        return;
+    }
+    else{
+        e.target.classList.add("warning")
+    }
+    
+    // let node = document.createElement("span")
+    // let textnode = document.createTextNode("bomb might be there")
+    // node.appendChild(textnode)
+    // node.className="child"
+    // document.getElementById(e.target.id).appendChild(node);
+
 }
+
+//display bombs 
 function displayBombs(){
-
-        bombArray.forEach(cell => {
-            setTimeout(function(){
-                document.getElementById(cell).classList.add(redselect);
-                console.log(document.getElementById(cell))
-                
-            },1000)
-            //console.log(cell)
-            setTimeout(function(){
-                document.getElementById(cell).classList.remove(redselect);
-                console.log(document.getElementById(cell))
-                
-            },2500)
-
-    })   
+    let i = 1;
+    bombArray.forEach(cell => {
+        (function(){
+            setTimeout(() => {
+                document.getElementById(cell).classList.add(redselect);                    
+            },i*200);
+        })();
+            i += 1;
+        });
 }
+
+// BombArray
 function createArray(){
     bombArray.push(Math.ceil(Math.random()*81))
     for(let i=0;i<8;i++){
@@ -158,154 +147,62 @@ function createArray(){
                 }
             }
     }
-    //console.log(createArray)
 }
-function hideBombs(){
-    bombArray.forEach(cell=>{
-        document.getElementById(cell).classList.remove(redselect);
-        //console.log(document.getElementById(cell))
-    })
-}
-function removeMouseEvent(e){
-    document.querySelectorAll(".cell")
-}
-function noOfBombs(bomb_val,cell,h)
-{
-    console.log("i clicked")
-    let temp = bomb_val;
-    let val = 0;
-    //console.log(typeof temp,temp)
-    
-    //console.log(clsname) 
-    //CORNER CASES
-    if (temp === 1){
-        
-        
-        if (bombArray.includes(temp+1)){val++;}
-        if (bombArray.includes(temp+9)){val++;}
-        if (bombArray.includes(temp+10)){val++;}
-        //console.log(cell)
-        return val;
-    }
-    //console.log(val)
-    if (temp === 9){
-        
-        if (bombArray.includes(temp-1)){val++;}
-        if (bombArray.includes(temp+8)){val++;}
-        if (bombArray.includes(temp+9)){val++;}
-        return val;
-    }
-    if (temp === 73){
-        
-        if (bombArray.includes(temp-9)){val++;}
-        if (bombArray.includes(temp-8)){val++;}
-        if (bombArray.includes(temp+1)){val++;}
-        return val;
-    }
-    if (temp === 81){
-        
-        if (bombArray.includes(temp-10)){val++;}
-        if (bombArray.includes(temp-9)){val++;}
-        if (bombArray.includes(temp-1)){val++;}
-        return val;
-    }
-    //Edge CASES
-    //Left
-    else if (temp % 9 == 1){
-        
 
-        if (bombArray.includes(temp-9)){val++;}
-        if (bombArray.includes(temp-8)){val++;}
-        if (bombArray.includes(temp+1)){val++;}
-        if (bombArray.includes(temp+9)){val++;}
-        if (bombArray.includes(temp+10)){val++;}
-        return val;
-    }
-    //Top
-    else if ((Math.floor(temp/9)) == 0){
-        
-        if (bombArray.includes(temp-1)){val++;}
-        if (bombArray.includes(temp+1)){val++;}
-        if (bombArray.includes(temp+8)){val++;}
-        if (bombArray.includes(temp+9)){val++;}
-        if (bombArray.includes(temp+10)){val++;}
-        return val;
-    }
-    //Right
-    else if (temp % 9 == 0){
-        
-        if (bombArray.includes(temp-10)){val++;}
-        if (bombArray.includes(temp-9)){val++;}
-        if (bombArray.includes(temp-1)){val++;}
-        if (bombArray.includes(temp+8)){val++;}
-        if (bombArray.includes(temp+9)){val++;}
-        return val;
-    }
-    //Bottom
-    else if ((Math.ceil(temp/9)) == 9){
-        
-        if (bombArray.includes(temp-10)){val++;}
-        if (bombArray.includes(temp-9)){val++;}
-        if (bombArray.includes(temp-8)){val++;}
-        if (bombArray.includes(temp-1)){val++;}
-        if (bombArray.includes(temp+1)){ val++;}
-        return val;
-    }
-    else{
-        
-        if (bombArray.includes(temp-10)){val++;}
-        if (bombArray.includes(temp-9)){val++;}
-        if (bombArray.includes(temp-8)){val++;}
-        if (bombArray.includes(temp-1)){val++;}
-        if (bombArray.includes(temp+1)){val++;}
-        if (bombArray.includes(temp+8)){val++;}
-        if (bombArray.includes(temp+9)){val++;}
-        if (bombArray.includes(temp+10)){val++;}
-        return val;
+//newBombArray with row and column
 
+function changeBombArray(){
+    console.log("i am new")
+    for(let i=0;i<bombArray.length;i++){
+        let row = Math.floor(bombArray[i]/9)
+        let col = (bombArray[i]-1) % 9
+        console.log(row,col)
+        newBombArray.push([row,col]);
     }
-    //console.log(val)
+
+}
+
+// Number of bombs in my rectangle box
+function noOfBombs(bomb_val,cell,h){
+    let temp = bomb_val
+    let row = Math.floor(temp/9)
+    let col = (temp-1)%9
+    let cnt = 0
+    let neighbors = [[row-1,col-1],[row-1,col],[row-1,col+1],[row,col-1],[row,col+1],[row+1,col-1],[row+1,col],[row+1,col+1]]
+    for(let i=0;i<neighbors.length;i++){
+        let r = neighbors[i][0]
+        let c = neighbors[i][1]
+        if(r >= 0 && r < 9 && c >= 0 && c < 9){
+            for(let j=0;j<newBombArray.length;j++){
+                if(newBombArray[j][0] === r && newBombArray[j][1] === c){
+                    cnt++;
+                }
+            }
+        }
+    }
+    return cnt;
 }
 
 
-// document.querySelectorAll(".greenselect").forEach(cell=>{
-//     console.log(cell)
-//     cell.addEventListener('mouseover',(event)=>{
-//         console.log(event.target)
-//         event.target.getElementById('id').classList.add('borderhover')
-//     })
-//     cell.addEventListener('mouseout',(event)=>{
-//         event.target.classList.remove('borderhover')
-//     })
-// })
 
+// for highlighting rectangle box
 function highlightClass(event)
 {
     console.log("i clicked")
     let temp = parseInt(event.target.id);
     let val = 0;
-    //console.log(typeof temp,temp)
-    
-    //console.log(clsname) 
+ 
     //CORNER CASES
     if (temp === 1){
-        console.log(typeof temp)
-            //console.log(cell.className)        
+        console.log(typeof temp)      
             event.target.classList.add("firstedge")
             document.getElementById(temp+1).classList.add("firstedgeright")
             document.getElementById(temp+9).classList.add("firstedgedown")
             document.getElementById(temp+10).classList.add("firstedgedownright")
             console.log(cell)
-            // console.log(document.getElementById(temp+1))
-            // console.log(document.getElementById(temp+9))
-            // console.log(document.getElementById(temp+10))
-        
-        
     }
-    //console.log(val)
+   
     if (temp === 9){
-    
-            //console.log(cell)
             event.target.classList.add("secondedge")
             document.getElementById(temp-1).classList.add("secondedgeleft")
             document.getElementById(temp+9).classList.add("secondedgedown")
@@ -313,7 +210,6 @@ function highlightClass(event)
         
     }
     if (temp === 73){
-            //console.log(cell)
             event.target.classList.add("thirdedge")
             document.getElementById(temp+1).classList.add("thirdedgeright")
             document.getElementById(temp-9).classList.add("thirdedgeup")
@@ -321,8 +217,6 @@ function highlightClass(event)
     
     }
     if (temp === 81){
-       
-            //console.log(cell)
             event.target.classList.add("fourthedge")
             document.getElementById(temp-1).classList.add("fourthedgeleft")
             document.getElementById(temp-9).classList.add("fourthedgeup")
@@ -332,17 +226,12 @@ function highlightClass(event)
     //Edge CASES
     //Left
     else if (temp % 9 == 1){
-
-            //console.log(cell)
             event.target.classList.add("leftedge")
             document.getElementById(temp+1).classList.add("leftedgeright")
             document.getElementById(temp+9).classList.add("leftedgedown")
             document.getElementById(temp+10).classList.add("leftedgedownright")
             document.getElementById(temp-8).classList.add("leftedgeupright")
             document.getElementById(temp-9).classList.add("leftedgeup")
-            
-        
-        
     }
     //Top
     else if ((Math.floor(temp/9)) == 0){
@@ -353,24 +242,18 @@ function highlightClass(event)
             document.getElementById(temp+10).classList.add("topedgedownright")
             document.getElementById(temp+8).classList.add("topedgedownleft")
             document.getElementById(temp-1).classList.add("topedgeleft")
-        
     }
     //Right
     else if (temp % 9 == 0){
-       
-            //console.log(cell)
             event.target.classList.add("rightedge")
             document.getElementById(temp+9).classList.add("rightedgedown")
             document.getElementById(temp-9).classList.add("rightedgeup")
             document.getElementById(temp-1).classList.add("rightedgeleft")
             document.getElementById(temp-10).classList.add("rightedgeleftup")
             document.getElementById(temp+8).classList.add("rightedgeleftdown")
-       
     }
     //Bottom
     else if ((Math.ceil(temp/9)) == 9){
-       
-            //console.log(cell)
             event.target.classList.add("bottomedge")
             document.getElementById(temp-1).classList.add("bottomleft")
             document.getElementById(temp+1).classList.add("bottomright")
@@ -380,8 +263,6 @@ function highlightClass(event)
        
     }
     else{
-        
-            //console.log(cell)
             event.target.classList.add("middle")
             document.getElementById(temp-1).classList.add("middleleft")
             document.getElementById(temp+1).classList.add("middleright")
@@ -393,46 +274,34 @@ function highlightClass(event)
             document.getElementById(temp+10).classList.add("middledownright")
 
         }
-    //console.log(val)
 }
+
 
 function highlightClass1(event)
 {
-    console.log("i clicked")
+
     let temp = parseInt(event.target.id);
     let val = 0;
-    //console.log(typeof temp,temp)
-    
-    //console.log(clsname) 
+   
     //CORNER CASES
     if (temp === 1){
-    
-        
             document.getElementById(temp).classList.remove("firstedge")
             document.getElementById(temp+1).classList.remove("firstedgeright")
             document.getElementById(temp+9).classList.remove("firstedgedown")
             document.getElementById(temp+10).classList.remove("firstedgedownright")
-            
-
     }
     //console.log(val)
     if (temp === 9){
-        
-       
             document.getElementById(temp).classList.remove("secondedge")
             document.getElementById(temp-1).classList.remove("secondedgeleft")
             document.getElementById(temp+9).classList.remove("secondedgedown")
             document.getElementById(temp+8).classList.remove("secondedgedownleft")
-            
-        
     }
     if (temp === 73){
-        
             document.getElementById(temp).classList.remove("thirdedge")
             document.getElementById(temp+1).classList.remove("thirdedgeright")
             document.getElementById(temp-9).classList.remove("thirdedgeup")
             document.getElementById(temp-8).classList.remove("thirdedgeupright")
-       
     }
     if (temp === 81){
         
@@ -440,8 +309,6 @@ function highlightClass1(event)
             document.getElementById(temp-1).classList.remove("fourthedgeleft")
             document.getElementById(temp-9).classList.remove("fourthedgeup")
             document.getElementById(temp-10).classList.remove("fourthedgeupleft")
-            
-    
     }
     //Edge CASES
     //Left
@@ -498,9 +365,6 @@ function highlightClass1(event)
             document.getElementById(temp+8).classList.remove("middledownleft")
             document.getElementById(temp+9).classList.remove("middledown")
             document.getElementById(temp+10).classList.remove("middledownright")
-            
-       
-
     }
     //console.log(val)
 }
